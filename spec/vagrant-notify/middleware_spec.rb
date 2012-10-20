572bf7f6e63399510b8a37fb8bea0b192b723023
Fabio Rehm <fgrehm@gmail.com>
2012-10-20 16:59:04 -0300

    Add spec to ensure that the middleware gets added to the right place

--- spec/vagrant-notify/middleware_spec.rb
+++ spec/vagrant-notify/middleware_spec.rb
@@ -1,8 +1,17 @@
 describe Vagrant::Notify::Middleware do
+  let(:start_stack) { Vagrant.actions[:start].send(:stack) }
+
   subject { described_class.new(@app, @env) }
 
   before do
     @app, @env = action_env(vagrant_env.vms.values.first.env)
+  end
+
+  it 'gets added after provision middleware for start action' do
+    provision_index  = start_stack.index([Vagrant::Action::VM::Provision, [], nil])
+    middleware_index = start_stack.index([Vagrant::Notify::Middleware, [], nil])
+
+    provision_index.should < middleware_index
   end
 
   it 'prints a hello world message when called' do
