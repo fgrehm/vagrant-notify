4f78577f3cd49ac827336b9d5669032cf8f1b1a4
Fabio Rehm <fgrehm@gmail.com>
2012-10-20 17:47:26 -0300

    Fork process for runing notification server on host machine

--- lib/vagrant-notify/server.rb
+++ lib/vagrant-notify/server.rb
@@ -1,21 +1,19 @@
 module Vagrant
   module Notify
     module Server
-      def post_init
-        puts "-- someone connected to the server!"
-      end
-
       def receive_data(data)
         `notify-send #{data}`
       end
 
       def self.run
         require 'eventmachine'
-
-        EventMachine::run {
-          # TODO: Add configuration for which port to use
-          EventMachine::start_server "127.0.0.1", 8081, self
-        }
+        fork do
+          $0 = 'vagrant-notify-server'
+          EventMachine::run {
+            # TODO: Add configuration for which port to use
+            EventMachine::start_server "127.0.0.1", 8081, self
+          }
+        end
       end
     end
   end
