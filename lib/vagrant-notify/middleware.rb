89f2433cd4da3079346866ef39abf32f8a929043
Fabio Rehm <fgrehm@gmail.com>
2012-10-20 18:57:55 -0300

    Ensure only one instance of the notification server is running

--- lib/vagrant-notify/middleware.rb
+++ lib/vagrant-notify/middleware.rb
@@ -6,16 +6,34 @@ module Vagrant
       end
 
       def call(env)
-        pid = Server.run
+        if pid = server_is_running?(env)
+          env[:ui].info "Notification server is already running (#{pid})"
+        else
+          pid = Server.run
 
-        local_data = env[:vm].env.local_data
-        local_data['vagrant-notify'] ||= Vagrant::Util::HashWithIndifferentAccess.new
-        local_data['vagrant-notify']['pid'] = pid
-        local_data.commit
+          local_data = env[:vm].env.local_data
+          local_data['vagrant-notify'] ||= Vagrant::Util::HashWithIndifferentAccess.new
+          local_data['vagrant-notify']['pid'] = pid
+          local_data.commit
 
-        env[:ui].info "Notification server fired up (#{pid})"
+          env[:ui].info "Notification server fired up (#{pid})"
+        end
 
         @app.call(env)
+      end
+
+      private
+
+      def server_is_running?(env)
+        begin
+          pid = env[:vm].env.local_data.fetch('vagrant-notify', {}).fetch('pid', nil)
+          return false unless pid
+
+          Process.getpgid(pid)
+          pid
+        rescue Errno::ESRCH
+          false
+        end
       end
     end
   end
--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -19,19 +19,54 @@ describe Vagrant::Notify::Middleware do
     provision_index.should > middleware_index
   end
 
-  it 'fires up notification server when called' do
-    Vagrant::Notify::Server.should_receive(:run)
-    subject.call(@env)
+  context 'server is down' do
+    before { Process.stub(:getpgid).and_raise(Errno::ESRCH) }
+
+    it 'fires up notification server when called' do
+      Vagrant::Notify::Server.should_receive(:run)
+      subject.call(@env)
+    end
+
+    it 'notifies user about server start' do
+      @env[:ui].should_receive(:info).with("Notification server fired up (#{server_pid})")
+      subject.call(@env)
+    end
+
+    it "persists server PID on local data" do
+      local_data.should_receive(:commit)
+      subject.call(@env)
+      local_data['vagrant-notify']['pid'].should == server_pid
+    end
+
+    context "and a PID is present on Vagrant's local data" do
+      let(:new_pid) { server_pid + 1 }
+
+      before do
+        Vagrant::Notify::Server.stub(:run => new_pid)
+        local_data['vagrant-notify'] = { 'pid' => server_pid }
+      end
+
+      it 'updates server PID' do
+        subject.call(@env)
+        local_data['vagrant-notify']['pid'].should == new_pid
+      end
+    end
   end
 
-  it 'notifies user about server start' do
-    @env[:ui].should_receive(:info).with("Notification server fired up (#{server_pid})")
-    subject.call(@env)
-  end
+  context 'server is up' do
+    before do
+      Process.stub(:getpgid => true)
+      local_data['vagrant-notify'] = { 'pid' => server_pid }
+    end
 
-  it "persists server PID on vagrant's local data" do
-    local_data.should_receive(:commit)
-    subject.call(@env)
-    local_data['vagrant-notify']['pid'].should == server_pid
+    it 'does not start a new server instance' do
+      Vagrant::Notify::Server.should_not_receive(:run)
+      subject.call(@env)
+    end
+
+    it 'notifies user that server is already running' do
+      @env[:ui].should_receive(:info).with("Notification server is already running (#{server_pid})")
+      subject.call(@env)
+    end
   end
 end
