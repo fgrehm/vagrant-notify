061bcb0c621d48c72a7af3fb20df55178117a779
Fabio Rehm <fgrehm@gmail.com>
2012-10-20 18:30:01 -0300

    Write server PID to vagrant local data

--- lib/vagrant-notify/middleware.rb
+++ lib/vagrant-notify/middleware.rb
@@ -7,7 +7,14 @@ module Vagrant
 
       def call(env)
         pid = Server.run
+
+        local_data = env[:vm].env.local_data
+        local_data['vagrant-notify'] ||= Vagrant::Util::HashWithIndifferentAccess.new
+        local_data['vagrant-notify']['pid'] = pid
+        local_data.commit
+
         env[:ui].info "Notification server fired up (#{pid})"
+
         @app.call(env)
       end
     end
--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -1,11 +1,14 @@
 describe Vagrant::Notify::Middleware do
   let(:start_stack) { Vagrant.actions[:start].send(:stack) }
   let(:server_pid)  { 1234 }
+  let(:local_data)  { Hash.new }
 
   subject { described_class.new(@app, @env) }
 
   before do
     @app, @env = action_env(vagrant_env.vms.values.first.env)
+    @env[:vm].env.stub(:local_data => local_data)
+    local_data.stub(:commit)
     Vagrant::Notify::Server.stub(:run => server_pid)
   end
 
@@ -24,5 +27,11 @@ describe Vagrant::Notify::Middleware do
   it 'notifies user about server start' do
     @env[:ui].should_receive(:info).with("Notification server fired up (#{server_pid})")
     subject.call(@env)
+  end
+
+  it "persists server PID on vagrant's local data" do
+    local_data.should_receive(:commit)
+    subject.call(@env)
+    local_data['vagrant-notify']['pid'].should == server_pid
   end
 end
