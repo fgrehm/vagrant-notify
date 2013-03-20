392a06a48564eb1e75313c92fef95947a25fded7
Fabio Rehm <fgrehm@gmail.com>
2013-03-20 01:28:20 -0300

    Add middleware to check if the server is running

---
+++ lib/vagrant-notify/action/server_is_running.rb
@@ -0,0 +1,28 @@
+module Vagrant
+  module Notify
+    module Action
+      class ServerIsRunning
+        def initialize(app, env)
+          @app    = app
+          @logger = Log4r::Logger.new("vagrant::notify::action::prepare_data_dir")
+        end
+
+        def call(env)
+          env[:result] = valid_process?(env[:notify_data][:pid])
+
+          # Call the next if we have one (but we shouldn't, since this
+          # middleware is built to run with the Call-type middlewares)
+          @app.call env
+        end
+
+        private
+
+        def valid_process?(pid)
+          Process.getpgid(pid) if pid
+        rescue Errno::ESRCH
+          false
+        end
+      end
+    end
+  end
+end
---
+++ spec/action/server_is_running_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+require 'vagrant-notify/action/server_is_running'
+
+describe Vagrant::Notify::Action::ServerIsRunning do
+  let(:app) { lambda { |env| } }
+  let(:env) { {notify_data: {pid: pid}} }
+
+  subject { described_class.new(app, env) }
+
+  context 'when pid is not set' do
+    let(:pid) { nil }
+
+    it 'sets the result to false' do
+      env[:result].should be_false
+    end
+  end
+
+  context 'when pid is set' do
+    let(:pid) { '12345' }
+
+    context 'and is valid' do
+      before do
+        Process.stub(:getpgid => true)
+        subject.call(env)
+      end
+
+      it 'sets the result to true' do
+        env[:result].should be_true
+      end
+    end
+
+    context 'and is invalid' do
+      before do
+        Process.stub(:getpgid).and_raise(Errno::ESRCH)
+        subject.call(env)
+      end
+
+      it 'sets the result to false' do
+        env[:result].should be_false
+      end
+    end
+  end
+end
