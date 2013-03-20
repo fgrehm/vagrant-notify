3e49775b9464615d4bb3f76f796d489295d54782
Fabio Rehm <fgrehm@gmail.com>
2013-03-20 00:55:11 -0300

    Add hash-like support to data object

--- lib/vagrant-notify/data.rb
+++ lib/vagrant-notify/data.rb
@@ -4,6 +4,15 @@ module Vagrant
       def initialize(data_dir)
         @data_dir = data_dir
       end
+
+      def []=(key, value)
+        @data_dir.join(key.to_s).open("w+") { |f| f.write(value) }
+      end
+
+      def [](key)
+        file = @data_dir.join(key.to_s)
+        file.read if file.file?
+      end
     end
   end
 end
---
+++ spec/data_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+require 'fileutils'
+require 'tmpdir'
+
+require 'vagrant-notify/data'
+
+describe Vagrant::Notify::Data do
+  let!(:data_dir) { Pathname.new(Dir.mktmpdir) }
+  let(:key)       { :pid }
+  let(:value)     { '123456' }
+
+  subject { described_class.new(data_dir) }
+
+  after { FileUtils.rm_rf data_dir.to_s }
+
+  it 'writes data out to a file' do
+    subject[key] = value
+
+    data_dir.join(key.to_s).read.should eq value
+  end
+
+  it 'reads data from files' do
+    data_dir.join(key.to_s).open('w+') { |f| f.write(value) }
+
+    subject[key].should eq value
+  end
+
+  it 'handles unset keys' do
+    subject[key].should be_nil
+  end
+end
