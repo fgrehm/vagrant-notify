9256ca08ed0fef3a1e3c2db06ff03882fa6e526e
Fabio Rehm <fgrehm@gmail.com>
2013-03-20 23:07:49 -0300

    Improve action hooks

--- lib/vagrant-notify/plugin.rb
+++ lib/vagrant-notify/plugin.rb
@@ -4,13 +4,14 @@ module Vagrant
       name 'vagrant notify'
       description 'Forwards notify-send from guest to host machine'
 
+      # TODO: This should be generic, we don't want to hard code every single
+      #       possible provider action class that Vagrant might have
       action_hook 'start-server-after-boot' do |hook|
         require_relative './action'
-        # TODO: This should be generic
         hook.after VagrantPlugins::ProviderVirtualBox::Action::Boot, Vagrant::Notify::Action.action_start_server
 
         if defined?(Vagrant::LXC)
-          # TODO: Require just the boot file once its "require dependencies" are sorted out
+          # TODO: Require just the boot action file once its "require dependencies" are sorted out
           require 'vagrant-lxc/action'
           hook.after Vagrant::LXC::Action::Boot, Vagrant::Notify::Action.action_start_server
         end
@@ -20,9 +21,22 @@ module Vagrant
         hook.after Vagrant::Action::Builtin::Provision, Vagrant::Notify::Action.action_start_server
       end
 
-      action_hook 'stop-server-after-halting' do |hook|
+      action_hook 'stop-server-after-halting', :machine_action_halt do |hook|
         require_relative './action'
         hook.after Vagrant::Action::Builtin::GracefulHalt, Vagrant::Notify::Action.action_stop_server
+      end
+
+      # TODO: This should be generic, we don't want to hard code every single
+      #       possible provider action class that Vagrant might have
+      action_hook 'stop-server-before-destruction', :machine_action_destroy do |hook|
+        require_relative './action'
+        hook.before VagrantPlugins::ProviderVirtualBox::Action::Destroy, Vagrant::Notify::Action.action_stop_server
+
+        if defined?(Vagrant::LXC)
+          # TODO: Require just the destroy action file once its "require dependencies" are sorted out
+          require 'vagrant-lxc/action'
+          hook.before Vagrant::LXC::Action::Destroy, Vagrant::Notify::Action.action_stop_server
+        end
       end
     end
   end
