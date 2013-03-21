465b9be812b62fced0248650d5ed14389489a324
Fabio Rehm <fgrehm@gmail.com>
2013-03-20 21:33:39 -0300

    Make sure everyone knows that CheckProvider is meant to be used along with vagrant's builtin Call action

--- lib/vagrant-notify/action/check_provider.rb
+++ lib/vagrant-notify/action/check_provider.rb
@@ -9,6 +9,8 @@ module Vagrant
         def call(env)
           env[:result] = true
 
+          # Call the next if we have one (but we shouldn't, since this
+          # middleware is built to run with the Call-type middlewares)
           @app.call env
         end
       end
