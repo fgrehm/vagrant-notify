2156a7264d8e42a305e1e4016c3077a55b34fc00
Fabio Rehm <fgrehm@gmail.com>
2013-06-04 01:45:28 -0300

    Rename action hooks

--- lib/vagrant-notify/plugin.rb
+++ lib/vagrant-notify/plugin.rb
@@ -17,18 +17,18 @@ module Vagrant
         end
       end
 
-      action_hook 'install-command-after-provisioning', :machine_action_provision do |hook|
+      action_hook 'install-command-after-provision', :machine_action_provision do |hook|
         hook.after Vagrant::Action::Builtin::Provision, Vagrant::Notify::Action.action_start_server
       end
 
-      action_hook 'stop-server-after-halting', :machine_action_halt do |hook|
+      action_hook 'stop-server-after-halt' do |hook|
         require_relative './action'
         hook.before Vagrant::Action::Builtin::GracefulHalt, Vagrant::Notify::Action.action_stop_server
       end
 
       # TODO: This should be generic, we don't want to hard code every single
       #       possible provider action class that Vagrant might have
-      action_hook 'stop-server-before-destruction', :machine_action_destroy do |hook|
+      action_hook 'stop-server-before-destroy', :machine_action_destroy do |hook|
         require_relative './action'
         hook.before VagrantPlugins::ProviderVirtualBox::Action::Destroy, Vagrant::Notify::Action.action_stop_server
 
