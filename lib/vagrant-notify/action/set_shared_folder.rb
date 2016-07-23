3ed07ce0b4eba2f4b809a29a7d215796dad7b46d
Alpha01 <root@rubyninja.org>
2016-07-22 21:14:57 -0700

    Adding support for Solaris and BSD guests

--- lib/vagrant-notify/action/set_shared_folder.rb
+++ lib/vagrant-notify/action/set_shared_folder.rb
@@ -7,12 +7,16 @@ module Vagrant
         end
 
         def call(env)         
-          unless env[:machine].config.notify.enable == false 
-            host_dir = Pathname("/tmp/vagrant-notify/#{env[:machine].id}")
-            FileUtils.mkdir_p host_dir.to_s unless host_dir.exist?
-            env[:machine].config.vm.synced_folder host_dir, "/tmp/vagrant-notify", id: "vagrant-notify"
+          begin
+            unless env[:machine].config.notify.enable == false 
+              host_dir = Pathname("/tmp/vagrant-notify/#{env[:machine].id}")
+              FileUtils.mkdir_p host_dir.to_s unless host_dir.exist?
+              env[:machine].config.vm.synced_folder host_dir, "/tmp/vagrant-notify", id: "vagrant-notify"
+            end
+            @app.call(env)
+            rescue
+              env[:machine].ui.warn("vagrant-notify: guest does not support the shared folder capability.")
           end
-          @app.call(env)
         end
       end
     end
