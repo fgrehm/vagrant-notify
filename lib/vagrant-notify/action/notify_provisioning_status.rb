f65b162078b122a749b432adad6ffb2e444d68af
Alpha01 <root@rubyninja.org>
2016-10-08 15:08:06 -0700

    Provision failed notice and disable provision notices if plugin has been disabled

--- lib/vagrant-notify/action/notify_provisioning_status.rb
+++ lib/vagrant-notify/action/notify_provisioning_status.rb
@@ -7,9 +7,18 @@ module Vagrant
         end
 
         def call(env)
-          system("notify-send '[#{env[:machine].name}] Provisioning with \"#{env[:provisioner_name]}\"...'")
-          @app.call(env)
-          system("notify-send '[#{env[:machine].name}] Finished provisioning with \"#{env[:provisioner_name]}\"'")
+
+          system("notify-send '[#{env[:machine].name}] Provisioning with \"#{env[:provisioner_name]}\"...'") unless env[:machine].config.notify.enable == false
+
+          begin
+            @app.call(env)
+          rescue => msg
+            system("notify-send '[#{env[:machine].name}] \"#{env[:provisioner_name]}\" provision failed!'") unless env[:machine].config.notify.enable == false
+            env[:machine].ui.error("#{msg}")
+          else
+            system("notify-send '[#{env[:machine].name}] Finished provisioning with \"#{env[:provisioner_name]}\"'") unless env[:machine].config.notify.enable == false
+          end
+
         end
       end
     end
